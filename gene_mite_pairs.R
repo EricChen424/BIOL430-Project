aaf4fcd940a23c8a5a5019ec0d3cbb200c84471a
echen424 <l0z9a@ugrad.cs.ubc.ca>
2018-12-03 15:14:11 -0800

    data cleaning protocol

--- gene_mite_pairs.R
+++ gene_mite_pairs.R
@@ -3,5 +3,11 @@ library(ggplot2)
 
 gene_mite_data <- read_csv("results/mite_gene_pairs.csv", col_names = TRUE)
 
-gene_mite_data %>% 
-  View()
+#remove all mites without a family or superfamily (it turns out all families have superfamilies in this dataset)
+#compute mite and gene length on the smaller dataset
+#select useful columns
+clean_data <- gene_mite_data %>% 
+  filter(!is.na(mite_family) | !is.na(mite_superfamily)) %>%
+  mutate(mite_length = mite_end - mite_start) %>%
+  mutate(gene_length = gene_end - gene_start) %>%
+  select(mite_name, mite_length, mite_family, mite_superfamily, gene_name, gene_length, distance)
